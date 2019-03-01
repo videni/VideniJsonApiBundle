f1dfb52da6659f786ed6ba890b275fcd47eac594
Vidy Videni <vidy.videni@gmail.com>
2019-03-01 17:33:40 +0800

    init repo

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Videni\Bundle\JsonApiBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('videni_json_api');
+
+        return $treeBuilder;
+    }
+}
---
+++ VideniJsonApiBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Videni\Bundle\JsonApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VideniJsonApiBundle extends Bundle
+{
+
+}
